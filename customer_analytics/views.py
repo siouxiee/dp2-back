8b90f00747f26dd497a022bade76dbacc389c2b3
Eduardo Izaguirre Pastor <89656213+eizag@users.noreply.github.com>
2024-10-08 15:11:52 -0500

    Views customer_analytics

--- customer_analytics/views.py
+++ customer_analytics/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Ciudad, Usuario, Producto, ProductoXUsuario, TipoProducto, Subcategoria
 
-# Create your views here.
+# Servicio para obtener la audiencia por ciudad
+def obtenerAudienciaPorCiudad(request):
+    ciudades = Ciudad.objects.all()
+    data = []
+    for ciudad in ciudades:
+        usuarios_ciudad = Usuario.objects.filter(direccion__distrito=ciudad.nombre).count()
+        data.append({
+            'ciudad': ciudad.nombre,
+            'audiencia': usuarios_ciudad
+        })
+    
+    return JsonResponse({'audiencia': data})
+
+# Servicio para obtener ventas por sabor
+def obtenerVentasPorSabor(request):
+    # Asumiendo que "sabores" están categorizados por Subcategoria
+    subcategorias = Subcategoria.objects.all()
+    ventas = {}
+    
+    for subcategoria in subcategorias:
+        productos = Producto.objects.filter(subcategoria=subcategoria)
+        cantidad = ProductoXUsuario.objects.filter(producto__in=productos).count()
+        ventas[subcategoria.nombre] = cantidad
+    
+    return JsonResponse({'ventas_sabores': ventas})
+
+def obtenerVentasPorProducto(request):
+    tipos_producto = TipoProducto.objects.all()
+    ventas = {}
+    
+    for tipo in tipos_producto:
+        productos_tipo = Producto.objects.filter(tipo_producto=tipo)
+        cantidad = ProductoXUsuario.objects.filter(producto__in=productos_tipo).count()
+        ventas[tipo.nombre] = cantidad
+    
+    return JsonResponse({'ventas_tamanos': ventas})
--- smmproject/settings.py
+++ smmproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TIKTOK_CLIENT_ID = config('TIKTOK_CLIENT_ID')
+TIKTOK_CLIENT_SECRET = config('TIKTOK_CLIENT_SECRET')
